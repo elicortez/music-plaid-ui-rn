4d9595938b2f46bd40253acf361fe9c900e9d708
Alisson Teixeira <alissonteixeira@hotmail.com>
2023-06-14 17:06:03 -0700

    fix urls

--- MusicPlaid/Config.js
+++ MusicPlaid/Config.js
@@ -1,11 +1,11 @@
 
 const Config = {
-    // userDataUrl: 'https://musicplaid.wm.r.appspot.com/user_data',
-    // songDataUrl: 'https://musicplaid.wm.r.appspot.com/song_data',
-    //spotifyDataUrl: 'http://musicplaid.wm.r.appspot.com/spotify_data',
-    userDataUrl: 'http://127.0.0.1:8080/user_data',
-    songDataUrl: 'http://127.0.0.1:8080/song_data',
-    spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
+    userDataUrl: 'https://musicplaid.wm.r.appspot.com/user_data',
+    songDataUrl: 'https://musicplaid.wm.r.appspot.com/song_data',
+    spotifyDataUrl: 'http://musicplaid.wm.r.appspot.com/spotify_data',
+    // userDataUrl: 'http://127.0.0.1:8080/user_data',
+    // songDataUrl: 'http://127.0.0.1:8080/song_data',
+    // spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
 }
 
 export default Config;
