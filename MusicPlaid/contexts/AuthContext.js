8bc54e5a547ba921ab584577f89c2cf51216dce4
Eli Cortez <elicorte@microsoft.com>
2023-06-26 20:52:07 -0400

    removing unecessary calls

--- MusicPlaid/contexts/AuthContext.js
+++ MusicPlaid/contexts/AuthContext.js
@@ -4,13 +4,12 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [spotifyProfile, setSpotifyProfile] = useState(null);
   const [appBackedInfo, setAppBackedInfo] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
   const [userData, setUserData] = useState(null);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, spotifyProfile, setSpotifyProfile, appBackedInfo, setAppBackedInfo, topArtists, setTopArtists, userData, setUserData}}>
+    <AuthContext.Provider value={{ user, setUser, appBackedInfo, setAppBackedInfo, topArtists, setTopArtists, userData, setUserData}}>
       {children}
     </AuthContext.Provider>
   );
