9b663746584c8a265a0c3b6b87d364a69c769416
Eli Cortez <elicorte@microsoft.com>
2023-06-28 17:46:31 -0400

    blocking the app in desktop

--- MusicPlaid/App.js
+++ MusicPlaid/App.js
@@ -7,6 +7,7 @@ import Profile from './screens/Profile';
 import Feed from './screens/Feed';
 import PeopleList from './screens/PeopleListScreen';
 import Music from './screens/Music';
+import { Image, View, Text } from 'react-native';
 
 const screenOption = {
   //headerShown: false,
@@ -19,15 +20,30 @@ const Stack = createStackNavigator();
 const App = () => {
   const { user } = useContext(AuthContext);
   const [loading, setLoading] = useState(true);
+  const [isDesktop, setIsDesktop] = useState(false);
 
   useEffect(() => {
     console.log('User state in App', user);
     setLoading(false);
+    
+    const userAgent = window.navigator.userAgent;
+    setIsDesktop(/Mobi/.test(userAgent) === false);
 
   }, [user]);
 
   if (loading) {
     return null;
+  }
+
+  if (isDesktop) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
+        <Image source={require('./assets/image1.png')} style={{ width: 300, height: 100 }} />
+        <Text style={{ textAlign: 'center', fontSize: 24, color: 'white' }}>Connecting people through music.</Text>
+        <View style={{ height: 10 }} /> {/* Add an extra line with 10 units of height */}
+        <Text style={{ textAlign: 'center', fontSize: 14, color: 'white' }}>Check our app in your mobile device.</Text>
+      </View>
+    );
   }
 
   return (
