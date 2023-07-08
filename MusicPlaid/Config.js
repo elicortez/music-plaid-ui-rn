27abc7331940a1f1f73cb180aedb616d355dbb01
Alisson Teixeira <alissonteixeira@hotmail.com>
2023-07-08 10:00:35 -0700

    show people compat

--- MusicPlaid/Config.js
+++ MusicPlaid/Config.js
@@ -1,17 +1,17 @@
 
 const Config = {
     userDataUrl: 'https://musicplaid.wm.r.appspot.com/user_data',
+    // userDataUrl: 'http://127.0.0.1:8080/user_data',
     songDataUrl: 'https://musicplaid.wm.r.appspot.com/song_data',
+    // songDataUrl: 'http://127.0.0.1:8080/song_data',
     spotifyDataUrl: 'http://musicplaid.wm.r.appspot.com/spotify_data',
+    // spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
     likeSongUrl: 'https://musicplaid.wm.r.appspot.com/like_track',
     unlikeSongUrl: 'https://musicplaid.wm.r.appspot.com/unlike_track',
     add_comment_url: 'https://musicplaid.wm.r.appspot.com/add_comment',
     follow_user: 'https://musicplaid.wm.r.appspot.com/follow_user',
     unfollow_user: 'https://musicplaid.wm.r.appspot.com/unfollow_user',
     search: 'https://musicplaid.wm.r.appspot.com/search',
-    // userDataUrl: 'http://127.0.0.1:8080/user_data',
-    // songDataUrl: 'http://127.0.0.1:8080/song_data',
-    // spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
 }
 
 export default Config;
--- MusicPlaid/components/PeopleList.js
+++ MusicPlaid/components/PeopleList.js
@@ -11,16 +11,25 @@ const PeopleList = ({ people, navigation }) => {
 
   // TODO: Use a FlatList react component instead of a map
   return (
-    <View style={{ alignItems: 'flex-start' }}>
+    <View style={{ flexDirection: 'column', alignItems: 'stretch' }}>
       {people.map((person, index) => (
-        <TouchableOpacity key={index} onPress={() => handlePersonPressed(person.id)}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', margin: 10 }}>
+        <TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} key={index} onPress={() => handlePersonPressed(person.id)}>
+          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', margin: 10 }}>
             <Image source={{ uri: person.img_url }} style={styles.imageThumbnail} />
             <Text style={{ color: 'white', fontSize: 15, marginLeft: 10 }}>{person.display_name}</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              {
+                person.compat_score ? (
+                  <Text style={{ flex: 1, color: 'white', fontSize: 15, textAlign: 'right', marginRight: 10 }}>Compat: {person.compat_score}</Text>
+                )
+                  : null
+              }
+            </View>
           </View>
         </TouchableOpacity>
-      ))}
-    </View>
+      ))
+      }
+    </View >
   )
 }
 
