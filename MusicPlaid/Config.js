04aafe5852f0164d17cf518831a535f1eeec0d91
Alisson Teixeira <alissonteixeira@hotmail.com>
2023-08-27 19:13:35 -0700

    base url to local machine

--- MusicPlaid/Config.js
+++ MusicPlaid/Config.js
@@ -1,17 +1,29 @@
+//const base = "https://musicplaid.wm.r.appspot.com/"
+const base = "http://127.0.0.1:8080/"
 
 const Config = {
-    userDataUrl: 'https://musicplaid.wm.r.appspot.com/user_data',
-    // userDataUrl: 'http://127.0.0.1:8080/user_data',
-    songDataUrl: 'https://musicplaid.wm.r.appspot.com/song_data',
-    // songDataUrl: 'http://127.0.0.1:8080/song_data',
-    spotifyDataUrl: 'http://musicplaid.wm.r.appspot.com/spotify_data',
-    // spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
-    likeSongUrl: 'https://musicplaid.wm.r.appspot.com/like_track',
-    unlikeSongUrl: 'https://musicplaid.wm.r.appspot.com/unlike_track',
-    add_comment_url: 'https://musicplaid.wm.r.appspot.com/add_comment',
-    follow_user: 'https://musicplaid.wm.r.appspot.com/follow_user',
-    unfollow_user: 'https://musicplaid.wm.r.appspot.com/unfollow_user',
-    search: 'https://musicplaid.wm.r.appspot.com/search',
+    userDataUrl: base + 'user_data',
+    songDataUrl: base + 'song_data',
+    spotifyDataUrl: base + 'spotify_data',
+    likeSongUrl: base + 'like_track',
+    unlikeSongUrl: base + 'unlike_track',
+    add_comment_url: base + 'add_comment',
+    follow_user: base + 'follow_user',
+    unfollow_user: base + 'unfollow_user',
+    search: base + 'search',
+
+    // userDataUrl: 'https://musicplaid.wm.r.appspot.com/user_data',
+    // // userDataUrl: 'http://127.0.0.1:8080/user_data',
+    // songDataUrl: 'https://musicplaid.wm.r.appspot.com/song_data',
+    // // songDataUrl: 'http://127.0.0.1:8080/song_data',
+    // spotifyDataUrl: 'http://musicplaid.wm.r.appspot.com/spotify_data',
+    // // spotifyDataUrl: 'http://127.0.0.1:8080/spotify_data',
+    // likeSongUrl: 'https://musicplaid.wm.r.appspot.com/like_track',
+    // unlikeSongUrl: 'https://musicplaid.wm.r.appspot.com/unlike_track',
+    // add_comment_url: 'https://musicplaid.wm.r.appspot.com/add_comment',
+    // follow_user: 'https://musicplaid.wm.r.appspot.com/follow_user',
+    // unfollow_user: 'https://musicplaid.wm.r.appspot.com/unfollow_user',
+    // search: 'https://musicplaid.wm.r.appspot.com/search',
 }
 
 export default Config;
